054e1c367612ddbc4504dbd557e23c783d7ddb49
Steven Johnstone <steven.james.johnstone@gmail.com>
2025-11-07 16:10:33 +0000

    Remove serialization from parse fuzzing

--- fuzz/parse.c
+++ fuzz/parse.c
@@ -2,8 +2,5 @@
 
 void
 harness(const uint8_t *input, size_t size) {
-    pm_buffer_t buffer;
-    pm_buffer_init(&buffer);
-    pm_serialize_parse(&buffer, input, size, NULL);
-    pm_buffer_free(&buffer);
+    pm_parse_success_p(input, size, NULL);
 }
