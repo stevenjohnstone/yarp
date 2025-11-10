d2ad549af04309a686236e084f67c3995a24fca7
Steven Johnstone <steven.james.johnstone@gmail.com>
2025-11-10 11:31:23 +0000

    Reduce fuzz input size to 128 bytes
    Improves fuzzer performance without any detectable loss of coverage.

--- fuzz/parse.sh
+++ fuzz/parse.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 OUTPUT_DIR=$1
+MAX_INPUT_SIZE=128
 
-screen -S grammar_fuzzer -d -m  /bin/bash -c "AFL_CUSTOM_MUTATOR_LIBRARY=/usr/local/lib/libgrammarmutator-ruby.so AFL_CUSTOM_MUTATOR_ONLY=1 afl-fuzz -i fuzz/corpus/parse -o $OUTPUT_DIR -S grammar -- ./build/fuzz.parse"
+screen -S grammar_fuzzer -d -m  /bin/bash -c "AFL_CUSTOM_MUTATOR_LIBRARY=/usr/local/lib/libgrammarmutator-ruby.so AFL_CUSTOM_MUTATOR_ONLY=1 afl-fuzz -G "$MAX_INPUT_SIZE" -i fuzz/corpus/parse -o "$OUTPUT_DIR" -S grammar -- ./build/fuzz.parse"
 
-screen -S main_fuzzer -m /bin/bash -c "afl-fuzz -x ./fuzz/dict -G 500 -c ./build/fuzz.parse.cmplog -i ./fuzz/corpus/parse -M main_parse -o $OUTPUT_DIR ./build/fuzz.parse || read -n 1"
+screen -S main_fuzzer -m /bin/bash -c "afl-fuzz -x ./fuzz/dict -G "$MAX_INPUT_SIZE" -c ./build/fuzz.parse.cmplog -i ./fuzz/corpus/parse -M main_parse -o "$OUTPUT_DIR" ./build/fuzz.parse || read -n 1"
 
 exec /bin/bash
